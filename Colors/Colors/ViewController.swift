1c1e4e89d9daec29c6c58073fbf09e2e67b6a5fc
mandalyn02 <fnq6@txstate.edu>
2025-03-17 13:45:22 -0500

    Add files via upload

---
+++ Colors/Colors/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Lewallen, Mandalyn E on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var navView: UIView!
+    
+    @IBAction func clickMenu(_ sender: UIBarButtonItem) {
+        if(navView.isHidden) {
+                  navView.isHidden = false
+              }
+              else {
+                  navView.isHidden = true
+              }
+          }
+    override func viewWillDisappear(_ animated: Bool) {
+          navView.isHidden = true
+      }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
